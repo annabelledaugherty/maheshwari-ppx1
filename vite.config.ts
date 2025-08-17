5cba02770772e0b74b1bd16e6c8280750f4b0598
mgpn1 <m.gpn1@mail.com>
2025-08-17 14:27:19 +0530

    yoursite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/maheshwari-ppx1/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
